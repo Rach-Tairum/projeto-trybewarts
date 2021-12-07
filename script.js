0ef5598dac2f648d0bf3a6d0345c6bdffcfef287
Rachel Tairum <rachel.tairum@gmail.com>
2021-12-07 19:04:00 -0300

    Requisito 21 com Linter ajustado

--- script.js
+++ script.js
@@ -42,80 +42,90 @@ const casas = document.getElementById('house');
 const conteudo = document.getElementsByClassName('subject');
 const comentario = document.getElementById('textarea');
 
+const escolhaConteudos = () => {
+  const selecionado = [];
+  // eslint-disable-next-line sonarjs/no-one-iteration-loop
+  for (let i = 0; i < conteudo.length; i += 1) {
+    if (conteudo[i].checked === true) {
+      selecionado.push(conteudo[i].value);
+    }
+  }
+  const string = selecionado.join(', ');
+  return string;
+};
+
+function apresentaBloco1(nomeSobrenome, emailValue) {
+  const bloco1 = document.createElement('section');
+  bloco1.id = 'dados iniciais';
+  form.appendChild(bloco1);
+
+  const name = document.createElement('p');
+  name.className = 'dados_texto';
+  name.innerText = `Nome: ${nomeSobrenome}`;
+  bloco1.appendChild(name);
+
+  const contato = document.createElement('p');
+  contato.className = 'dados_texto';
+  contato.innerText = `Email: ${emailValue}`;
+  bloco1.appendChild(contato);
+}
+
+function apresentaBloco2(escolhaCasa, escolhaFamilia, string, nota) {
+  const bloco2 = document.createElement('section');
+  bloco2.id = 'escolhas';
+  form.appendChild(bloco2);
+
+  const casa = document.createElement('p');
+  casa.className = 'dados_texto';
+  casa.innerText = `Casa: ${escolhaCasa}`;
+  bloco2.appendChild(casa);
+
+  const familia = document.createElement('p');
+  familia.className = 'dados_texto';
+  familia.innerText = `Família: ${escolhaFamilia}`;
+  bloco2.appendChild(familia);
+
+  const conteudos = document.createElement('p');
+  conteudos.className = 'dados_texto';
+  conteudos.innerText = `Matérias: ${string}`;
+  bloco2.appendChild(conteudos);
+
+  const avaliacao = document.createElement('p');
+  avaliacao.className = 'dados_texto';
+  avaliacao.innerText = `Avaliação: ${nota}`;
+  bloco2.appendChild(avaliacao);
+}
+
+function apresentaBloco3(observa) {
+  const bloco3 = document.createElement('section');
+  bloco3.id = 'observacoes';
+  form.appendChild(bloco3);
+
+  const observacao = document.createElement('article');
+  observacao.className = 'dados_texto';
+  observacao.innerText = `Observações: ${observa}`;
+  bloco3.appendChild(observacao);
+}
+
 function textoCompleto() {
   submit.addEventListener('click', (event) => {
     event.preventDefault();
     const nomeValor = nome.value;
     const sobrenomeValue = ultimoNome.value;
+    const nomeSobrenome = `${nomeValor} ${sobrenomeValue}`;
+
     const emailValue = email.value;
     const escolhaCasa = casas.options[casas.selectedIndex].value;
     const escolhaFamilia = form.family.value;
-    const escolhaConteudos = () => {
-      const selecionado = [];
-      // eslint-disable-next-line sonarjs/no-one-iteration-loop
-      for (let i = 0; i < conteudo.length; i += 1) {
-        if (conteudo[i].checked === true) {
-          selecionado.push(conteudo[i].value);
-        }
-      }
-      const string = selecionado.join(', ');
-      return string;
-    };
     const string = escolhaConteudos();
     const nota = form.rate.value;
     const observa = comentario.value;
 
     form.innerHTML = '';
 
-    function apresentaTexto() {
-      const bloco1 = document.createElement('section');
-      bloco1.id = 'dados iniciais';
-      form.appendChild(bloco1);
-
-      const nomeSobrenome = document.createElement('p');
-      nomeSobrenome.className = 'dados_texto';
-      nomeSobrenome.innerText = `Nome: ${nomeValor} ${sobrenomeValue}`;
-      bloco1.appendChild(nomeSobrenome);
-
-      const contato = document.createElement('p');
-      contato.className = 'dados_texto';
-      contato.innerText = `Email: ${emailValue}`;
-      bloco1.appendChild(contato);
-
-      const bloco2 = document.createElement('section');
-      bloco2.id = 'escolhas';
-      form.appendChild(bloco2);
-
-      const casa = document.createElement('p');
-      casa.className = 'dados_texto';
-      casa.innerText = `Casa: ${escolhaCasa}`;
-      bloco2.appendChild(casa);
-
-      const familia = document.createElement('p');
-      familia.className = 'dados_texto';
-      familia.innerText = `Família: ${escolhaFamilia}`;
-      bloco2.appendChild(familia);
-
-      const conteudos = document.createElement('p');
-      conteudos.className = 'dados_texto';
-      conteudos.innerText = `Matérias: ${string}`;
-      bloco2.appendChild(conteudos);
-
-      const avaliacao = document.createElement('p');
-      avaliacao.className = 'dados_texto';
-      avaliacao.innerText = `Avaliação: ${nota}`;
-      bloco2.appendChild(avaliacao);
-
-      const bloco3 = document.createElement('section');
-      bloco3.id = 'observacoes';
-      form.appendChild(bloco3);
-
-      const observacao = document.createElement('article');
-      observacao.className = 'dados_texto';
-      observacao.innerText = `Observações: ${observa}`;
-      bloco3.appendChild(observacao);
-    }
-    apresentaTexto();
+    apresentaBloco1(nomeSobrenome, emailValue);
+    apresentaBloco2(escolhaCasa, escolhaFamilia, string, nota);
+    apresentaBloco3(observa);
   });
 }
 textoCompleto();
